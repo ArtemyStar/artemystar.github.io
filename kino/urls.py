ca09e87b910704c1d853d3a2633579308f6a1e21
Аrtemy <artemy.terentiev2009@gmail.com>
2023-11-05 13:59:36 +0200

    исправление ошибок

--- kino/urls.py
+++ kino/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from kino import views
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
